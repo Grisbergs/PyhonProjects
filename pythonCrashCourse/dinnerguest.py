a48c53f39f1f65f7529f11a2dfa236aa5dd711d4
Grisbergs <44629013+Grisbergs@users.noreply.github.com>
2025-02-28 23:15:37 -0500

    updated  for 3-5

--- pythonCrashCourse/dinnerguest.py
+++ pythonCrashCourse/dinnerguest.py
@@ -1,3 +1,10 @@
 #page 41 3-4
 guests =['Ralph','Ryan','Kurt','John','William']
 #testpush
+print(guests)
+print ("This is and invitation to dinner for "+ guests[0])
+print ("This is and invitation to dinner for "+ guests[1])
+print ("This is and invitation to dinner for "+ guests[2])
+print ("This is and invitation to dinner for "+ guests[3])
+print ("This is and invitation to dinner for "+ guests[4])
+
