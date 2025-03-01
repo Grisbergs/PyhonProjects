fd0ff783ac59b4f43b9601609c6841c779f947c7
Grisbergs <44629013+Grisbergs@users.noreply.github.com>
2025-03-01 15:24:21 -0500

    page 41 and 45 exercises

--- pythonCrashCourse/dinnerguest.py
+++ pythonCrashCourse/dinnerguest.py
@@ -18,3 +18,21 @@ print ("This is and invitation to dinner for "+ guests[1])
 print ("This is and invitation to dinner for "+ guests[2])
 print ("This is and invitation to dinner for "+ guests[3])
 print ("This is and invitation to dinner for "+ guests[4])
+
+# page 45  3-9 
+numberOfGuests = len(guests)
+print(numberOfGuests)
+
+#page 45 3-10  
+cities = ['Atlanta','Chicago','New York City','Houston','New Orleans','Miami']
+cities.sort(reverse= True)
+print(cities)
+
+print(sorted(cities))
+cities.reverse()
+print(cities)
+cityCount = len(cities)
+print(cityCount)
+
+
+
