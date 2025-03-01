aa7cf098e88330a3460fe11adc37760b6ca82aae
Grisbergs <44629013+Grisbergs@users.noreply.github.com>
2025-02-28 23:19:15 -0500

    3-5 continued

--- pythonCrashCourse/dinnerguest.py
+++ pythonCrashCourse/dinnerguest.py
@@ -8,3 +8,13 @@ print ("This is and invitation to dinner for "+ guests[2])
 print ("This is and invitation to dinner for "+ guests[3])
 print ("This is and invitation to dinner for "+ guests[4])
 
+#3-5
+print ( "John Can't maket it we will invite Sam instead.")
+guests.remove("John")
+guests.append("Sam")
+print(guests)
+print ("This is and invitation to dinner for "+ guests[0])
+print ("This is and invitation to dinner for "+ guests[1])
+print ("This is and invitation to dinner for "+ guests[2])
+print ("This is and invitation to dinner for "+ guests[3])
+print ("This is and invitation to dinner for "+ guests[4])
